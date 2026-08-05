3ab17295aee6015945142cd2e4f37359e83000f2
kaherro <torkomyanvv@gmail.com>
2026-08-05 22:21:15 +0300

    [feat]: Realized basic functions (constructor, loading cities, random city, city by name, angle and distance between 2 cities)

--- src/db_manager.cpp
+++ src/db_manager.cpp
@@ -1,27 +1,143 @@
 #include "db_manager.h"
 #include <stdexcept>
+#include <cmath>
+#include <random>
+#include <algorithm>
 
-db_manager::db_manager(const std::string &db_path) {
-    // open sqlite
+db_manager::db_manager(const std::string &db_path = "cities.db") {
+    sqlite3 *raw_db = nullptr; 
+    if (sqlite3_open(db_path.c_str(), &raw_db) != SQLITE_OK) {
+        const char* error_msg = "unknown error"; 
+        if(raw_db) error_msg = sqlite3_errmsg(raw_db); 
+        throw std::runtime_error(
+            "Error opening database " + db_path + ": " + error_msg
+        );
+    }
+    db.reset(raw_db);
+    load_all_cities(); 
 }
-db_manager::~db_manager() {
-    // close sqlite
-};
+
+// void db_manager::exec(const char *sql) {
+//     char *error_msg = nullptr; 
+//     if(sqlite3_exec(db.get(), sql, nullptr, nullptr, &error_msg) != SQLITE_OK) {
+//         std::string s = (error_msg ? error_msg : "unknow error");
+//         sqlite3_free(error_msg);
+//         throw std::runtime_error("SQL Error: " + s); 
+//     }
+// }
+
+void db_manager::load_all_cities() {
+    const char* sql = "SELECT * FROM cities;";
+    sqlite3_stmt* raw_stmt = nullptr;
+    auto stmt_deleter = [](sqlite3_stmt* s) { sqlite3_finalize(s); };
+    if (sqlite3_prepare_v2(db.get(), sql, -1, &raw_stmt, nullptr) != SQLITE_OK) {
+        throw std::runtime_error("Failed to read all cities: " + *sqlite3_errmsg(db.get()));
+    }
+    std::unique_ptr<sqlite3_stmt, decltype(stmt_deleter)> stmt(raw_stmt, stmt_deleter);
+    
+    while (sqlite3_step(stmt.get()) == SQLITE_ROW) {
+        city city_; 
+        city_.id = sqlite3_column_int64(stmt.get(), 0);
+        city_.name = reinterpret_cast<const char*>(sqlite3_column_text(stmt.get(), 1));
+        city_.latitude = sqlite3_column_double(stmt.get(), 2);
+        city_.longitude = sqlite3_column_double(stmt.get(), 3);
+        city_.country = reinterpret_cast<const char*>(sqlite3_column_text(stmt.get(), 4));
+        cities.push_back(city_);
+        std::string name_ = city_.name; 
+        std::transform(name_.begin(), name_.end(), name_.begin(), ::tolower); 
+        name_to_city[name_] = city_; 
+    }
+}
 
 city db_manager::get_random_city() {
-    city x; 
-    x.id = 1; 
-    x.name = "London"; 
-    x.latitude = 51.5074;
-    x.longitude = -0.1278;
-    x.country = "United Kingdom";
-    return x;
+    if(cities.empty()) {
+        throw std::runtime_error("Cities table is empty"); 
+    }
+    std::random_device rd; 
+    std::mt19937 gen(rd()); 
+    std::uniform_int_distribution<int> distrib(1, 9999999); 
+    return cities[distrib(gen) % (int)cities.size()]; 
+
+    // const char *sql = "SELECT * FROM cities ORDER BY RANDOM() LIMIT 1;";
+    // city city_; 
+
+    // sqlite3_stmt* raw_stmt = nullptr;
+    // auto stmt_deleter = [](sqlite3_stmt* s) { sqlite3_finalize(s); };
+    // if (sqlite3_prepare_v2(db.get(), sql, -1, &raw_stmt, nullptr) != SQLITE_OK) {
+    //     throw std::runtime_error("Failed to get random city: " + *sqlite3_errmsg(db.get()));
+    // }
+    // std::unique_ptr<sqlite3_stmt, decltype(stmt_deleter)> stmt(raw_stmt, stmt_deleter);
+
+    // if(sqlite3_step(stmt.get()) == SQLITE_OK) {
+    //     city_.id = sqlite3_column_int64(stmt.get(), 0);
+    //     city_.name = reinterpret_cast<const char*>(sqlite3_column_text(stmt.get(), 1));
+    //     city_.latitude = sqlite3_column_double(stmt.get(), 2);
+    //     city_.longitude = sqlite3_column_double(stmt.get(), 3);
+    //     city_.country = reinterpret_cast<const char*>(sqlite3_column_text(stmt.get(), 4));
+    // }
+    // else {
+    //     sqlite3_finalize(stmt.get());
+    //     throw std::runtime_error("City table is empty"); 
+    // }
+    // sqlite3_finalize(stmt.get());
+    // return city_;
 }
 
 std::optional<city> db_manager::get_city_by_name(const std::string &name) {
-    return std::nullopt; 
+    std::string name_ = name; 
+    std::transform(name_.begin(), name_.end(), name_.begin(), ::tolower); 
+    if(name_to_city.contains(name)) {
+        return name_to_city[name];
+    }
+    return std::nullopt;
+    // const char *sql = "SELECT * FROM cities WHERE LOWER(name) = LOWER(?) LIMIT 1;";
+    // city city_; 
+
+    // sqlite3_stmt* raw_stmt = nullptr;
+    // auto stmt_deleter = [](sqlite3_stmt* s) { sqlite3_finalize(s); };
+    // if (sqlite3_prepare_v2(db.get(), sql, -1, &raw_stmt, nullptr) != SQLITE_OK) {
+    //     throw std::runtime_error("Failed to get city by name: " + *sqlite3_errmsg(db.get()));
+    // }
+    // std::unique_ptr<sqlite3_stmt, decltype(stmt_deleter)> stmt(raw_stmt, stmt_deleter);
+
+    // if(sqlite3_step(stmt.get()) == SQLITE_OK) {
+    //     city_.id = sqlite3_column_int64(stmt.get(), 0);
+    //     city_.name = reinterpret_cast<const char*>(sqlite3_column_text(stmt.get(), 1));
+    //     city_.latitude = sqlite3_column_double(stmt.get(), 2);
+    //     city_.longitude = sqlite3_column_double(stmt.get(), 3);
+    //     city_.country = reinterpret_cast<const char*>(sqlite3_column_text(stmt.get(), 4));
+    // }
+    // else {
+    //     sqlite3_finalize(stmt.get());
+    //     throw std::runtime_error("City table is empty"); 
+    // }
+    // sqlite3_finalize(stmt.get());
+    // return city_;    
 }
 
-db_manager::vector_geo db_manager::count_vector(const city &from, const city &to) {
-    return {0.0, 0.0}; 
+static inline double deg_to_rad(double x) {
+    return x * M_PI / 180.0; 
+}
+
+static inline double rad_to_deg(double x) {
+    return x * 180.0 / M_PI; 
+}
+
+const db_manager::vector_geo db_manager::count_vector(const city &from, const city &to) {
+    double lat1 = deg_to_rad(from.latitude);
+    double lat2 = deg_to_rad(to.latitude);
+    double lon1 = deg_to_rad(from.longitude);
+    double lon2 = deg_to_rad(to.longitude);
+
+    double dlon = lon2 - lon1; 
+    double y = std::sin(dlon) * std::cos(lat2);
+    double x = std::cos(lat1) * std::sin(lat2) - std::sin(lat1) * std::cos(lat2) * std::cos(dlon);
+    double angle = rad_to_deg(std::atan2(y, x));
+    if(angle < 0.0) angle += 360.0;
+
+    double haversine = std::sin((lat2 - lat1) / 2) * std::sin((lat2 - lat1) / 2) + 
+                    std::cos(lat1) * std::cos(lat2) * std::sin(dlon / 2) * std::sin(dlon / 2);
+    double distance = 2.0 * 6371.0 * std::asin(std::sqrt(haversine));
+
+    return {distance, angle};
 }
--- src/db_manager.h
+++ src/db_manager.h
@@ -4,6 +4,7 @@
 #include <optional>
 #include <sqlite3.h>
 #include <memory>
+#include <unordered_map>
 
 struct city {
     long long id{};
@@ -16,7 +17,6 @@ struct city {
 class db_manager {
 public:
     explicit db_manager(const std::string &db_path);
-    ~db_manager();
 
     city get_random_city(); 
     std::optional<city> get_city_by_name(const std::string &name); 
@@ -25,7 +25,8 @@ public:
         double distance_km;
         double angle_deg; 
     };
-    vector_geo count_vector(const city &from, const city &to);
+    const vector_geo count_vector(const city &from, const city &to);
+
 private:
     struct sqlite3_deleter {
         void operator()(sqlite3* db) const {
@@ -33,5 +34,8 @@ private:
         }
     };
     std::unique_ptr<sqlite3, sqlite3_deleter> db;
-    void exec(const char *sql); 
+    void load_all_cities();
+    // void exec(const char *sql); 
+    std::unordered_map<std::string, city> name_to_city; 
+    std::vector<city> cities; 
 };
